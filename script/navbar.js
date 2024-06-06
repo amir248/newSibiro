8416947151dd5e8fdb409cbe291c437f1d5f6639
chikchicly <chikchicly@gmail.com>
2024-06-06 18:10:19 +0700

    oK

--- script/navbar.js
+++ script/navbar.js
@@ -5,23 +5,26 @@ const menuBtn = document.querySelector(".menu--btn");
 const menuCloseBtn = document.querySelector(".menu--close-btn");
 const discountLink = document.querySelector(".discount--link");
 
-menuBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log("CLICK");
-  menuArea.style.transform = "translateX(0)";
-  menuArea.style.opacity = "1";
-});
-menuCloseBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log("CLICK");
-  menuArea.style.transform = "translateX(-110%)";
-  menuArea.style.opacity = "0";
-});
-discountLink.addEventListener("click", function (e) {
-  e.preventDefault();
-  menuArea.style.transform = "translateX(-110%)";
-  menuArea.style.opacity = "0";
-  document
-    .getElementById("discountsAndPromotions")
-    .scrollIntoView({ behavior: "smooth" });
-});
+window.addEventListener('DOMContentLoaded',oKfunction);
+function oKfunction(){
+  menuBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log("CLICK");
+    menuArea.style.transform = "translateX(0)";
+    menuArea.style.opacity = "1";
+  });
+  menuCloseBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log("CLICK");
+    menuArea.style.transform = "translateX(-110%)";
+    menuArea.style.opacity = "0";
+  });
+  discountLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    menuArea.style.transform = "translateX(-110%)";
+    menuArea.style.opacity = "0";
+    document
+      .getElementById("discountsAndPromotions")
+      .scrollIntoView({ behavior: "smooth" });
+  });
+};//oKfunction
